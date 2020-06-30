fac5b24ad2b818432fc8b162b811f457b9c30dba
Doug Parker <dgp1130@users.noreply.github.com>
2020-06-30 15:22:22 -0700

    Fixing usage of getContent().

--- panel.js
+++ panel.js
@@ -1,13 +1,7 @@
 chrome.devtools.network.onRequestFinished.addListener((connection) => {
-  connection.getContent((data) => {
-    // Limit to 10 characters to not blow up the console.
-    if (!data) {
-      console.log('Got bad data', data);
-    } else {
-      const [content, encoding] = data;
-      if (content === null || content === undefined) {
-        console.log('Got bad content ', content);
-      }
+  connection.getContent((content, encoding) => {
+    if (content === null || content === undefined) {
+      console.log(`Got bad content (${content}) for URL: ${connection.request.url}`);
     }
   });
 });
